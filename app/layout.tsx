787e094a61b853512ff8de1f0ce359a403736d9a
xusongliang <xusl_1979@hotmail.com>
2023-12-20 10:28:16 +0800

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css'; //全局css
+import { inter } from '@/app/ui/fonts'; //全局字体
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  //metadata <head> 头部 信息
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
